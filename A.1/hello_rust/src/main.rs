4afa9dd3b9a60768534306e7732ab0e28853a377
ajitirto <ajiwoke@gmail.com>
2022-12-26 11:34:37 +0700

    :smiley: oher other configuraion

--- A.1/hello_rust/src/main.rs
+++ A.1/hello_rust/src/main.rs
@@ -128,8 +128,15 @@
 
 
 // A.7
+
+// satic keyword
+static NUMBER: i32 = 18;
+
 fn main() {
     const LABEL: &str = "nilai pi adalah : ";
     const PI :f32 = 22.0/7.0;
     println!("{} {}", LABEL, PI); 
+
+    println!("{}", NUMBER);
+    
 }
