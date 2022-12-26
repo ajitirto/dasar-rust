e9765e1eb3c3f324d7d2764b2fed9ae6f75ac96e
ajitirto <ajiwoke@gmail.com>
2022-12-26 09:41:16 +0700

    :smiley: rust package

--- A.1/hello_rust/src/main.rs
+++ A.1/hello_rust/src/main.rs
@@ -1,3 +1,16 @@
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    // let nama = "Aji Tirto Prayogo";
+    // println!("{}",nama);
+
+
+    let mut  angka= 1;
+    let pesan = "pesan dari @ajitirto";
+    println!(" No - {} - {}", angka, pesan);
+
+    angka= 2;
+    let pesan = "pesan dari @ajiwoke";
+    println!(" No - {} - {}", angka, pesan);
+
 }
