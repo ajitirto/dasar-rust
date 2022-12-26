8a772a5bb71790d6f04c055e18e05e742b68a0f0
ajitirto <ajiwoke@gmail.com>
2022-12-26 08:51:50 +0700

    :smiley: add target and cargo.lock

---
+++ A.1/hello_rust/target/debug/deps/hello_rust-f452b10df228853b.d
@@ -0,0 +1,5 @@
+/home/ajitirto/maintenance-github/dasarRust/A.1/hello_rust/target/debug/deps/hello_rust-f452b10df228853b: src/main.rs
+
+/home/ajitirto/maintenance-github/dasarRust/A.1/hello_rust/target/debug/deps/hello_rust-f452b10df228853b.d: src/main.rs
+
+src/main.rs:
---
+++ A.1/hello_rust/target/debug/hello_rust.d
@@ -0,0 +1,1 @@
+/home/ajitirto/maintenance-github/dasarRust/A.1/hello_rust/target/debug/hello_rust: /home/ajitirto/maintenance-github/dasarRust/A.1/hello_rust/src/main.rs
