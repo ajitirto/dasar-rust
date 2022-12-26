2acd265743ebd6ee4c62de14387e13e4b925a081
ajitirto <ajiwoke@gmail.com>
2022-12-26 11:09:00 +0700

    :smiley: add main.rs bab 5

--- A.1/hello_rust/src/main.rs
+++ A.1/hello_rust/src/main.rs
@@ -54,3 +54,45 @@
 
 
 // A.5
+// fn main() {
+//     // sign integer
+//     let number1:i8 = 12;
+//     let number2:i8 = 22;
+//     let number3:i32 = 148; // 128 => i8 128 > i32
+
+//     println!("{}-{}-{}", number1,number2,number3);
+
+//     // unsign integer => tidak menampung value negatif
+//     let number4:i32 = 12;
+//     let number5:i8 = 22;
+//     let number6:i64 = 148; // 128 => i8 128 > i32
+
+//     println!("{}-{}-{}", number4,number5,number6);
+
+
+//     // floating point 
+
+//     let angka1:f32 = 3.14;
+//     let angka2:f64 = 3.1412312312;
+
+//     println!("{}|{:.5}",angka1,angka2);
+
+//     // Bool
+//     let b1= true;
+//     let b2= false;
+
+//     println!("{}|{}",b1,b2);
+
+//     // char
+//     let c1='n';
+//     let c2 = '-';
+//     let c3 = '2';
+
+//     println!("{}|{}|{}", c1,c2,c3);
+
+//     // porinter scalar
+
+//     let pointer: &i32 = &24;
+//     println!("{}", pointer);
+
+// }
