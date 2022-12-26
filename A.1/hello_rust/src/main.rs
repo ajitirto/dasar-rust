0b409e16cf7c8fc95c2a19b6cee2a0029f892e59
ajitirto <ajiwoke@gmail.com>
2022-12-26 10:28:02 +0700

    :smiley: add main.rs bab 4

--- A.1/hello_rust/src/main.rs
+++ A.1/hello_rust/src/main.rs
@@ -1,16 +1,56 @@
 
-fn main() {
-    // println!("Hello, world!");
-    // let nama = "Aji Tirto Prayogo";
-    // println!("{}",nama);
+// fn main() {
+//     // println!("Hello, world!");
+//     // let nama = "Aji Tirto Prayogo";
+//     // println!("{}",nama);
 
 
-    let mut  angka= 1;
-    let pesan = "pesan dari @ajitirto";
-    println!(" No - {} - {}", angka, pesan);
+//     let mut  angka= 1;
+//     let pesan = "pesan dari @ajitirto";
+//     println!(" No - {0} - {1}", angka, pesan);
 
-    angka= 2;
-    let pesan = "pesan dari @ajiwoke";
-    println!(" No - {} - {}", angka, pesan);
+//     angka= 2;
+//     let pesan = "pesan dari @ajiwoke";
+//     println!(" No - {} - {}", angka, pesan);
 
-}
+//     angka= 3;
+//     let pesan = 24;
+//     println!(" No - {1} - {0}", angka, pesan);
+
+//     // metode tyoe "interface" 
+//     let var1 = 12;
+//     let var2 = 12;
+//     // metode type manifest => menulis secara eskplisit
+//     let message: i8 = 24; 
+//     println!(
+//         "{}-{}-{}",
+//         var1,
+//         var2,
+//         message
+//     );
+
+
+// }
+
+// A.4.7
+// fn main() {
+//     let(var1, var2) = (24, "hello");
+//     println!("var1: {0}", var1);
+//     println!("var2: {0}",var2);
+
+//     let(var3, var4):(i8,i8) = (31,24);
+//     println!("var3: {0}", var3);
+//     println!("var4: {0}",var4);
+
+//     let data1  = 28_i8;
+//     println!("data1 {0}", data1);
+
+//     let x = 5;
+//     println!{"x {}", x};
+
+//     let x = x +1;
+//     println!("x+1: {}", x);
+// }
+
+
+// A.5
